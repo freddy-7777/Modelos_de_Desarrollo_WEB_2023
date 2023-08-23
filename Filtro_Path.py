547dbe9536388b6f77138b6b104043a0005aa92f
Freddy García <122243349+freddy-7777@users.noreply.github.com>
2023-08-23 14:19:00 -0600

    Filtro por Path

---
+++ Filtro_Path.py
@@ -0,0 +1,42 @@
+#Importamos el framework fastapi a nuestro entorno de trabajo
+from fastapi import FastAPI 
+#Importamos pydantic para obtener una entidad que pueda definir usuarios
+from pydantic import BaseModel
+
+#Creamos un objeto a partir de la clase FastAPI
+app= FastAPI()
+
+#Levantamos el server Uvicorn
+#-uvicorn 3_crud:app --reload-
+#{"id":3,"Name":"Alfredo", "LastName":"Garcia", "Age":30}
+#Definimos nuestra entidad: User
+
+class User(BaseModel):
+    id:int
+    Name: str
+    LastName:str
+    Age:int
+    
+#Creamos un objeto en forma de lista con diferentes usuarios (Esto sería una base de datos)  
+users_list= [User(id=0,Name="Alfredo", LastName="Garcia", Age="30"),
+             User(id=1,Name="Juan", LastName="Perez", Age="45"),
+             User(id=2,Name="María", LastName="Lopez", Age="22")]
+
+
+#***Get
+@app.get("/usersclass/")
+async def usersclass():
+    return (users_list)
+ # En el explorador colocamos la raiz de la ip: http://127.0.0.1:8000/usersclass/
+
+
+#***Get con Filtro Path
+@app.get("/usersclass/{id}")
+async def usersclass(id:int):
+    users=filter (lambda user: user.id == id, users_list)  #Función de orden superior
+    try:
+        return list(users)[0]
+    except:
+        return{"error":"No se ha encontrado el usuario"}
+    
+     # En el explorador colocamos la raiz de la ip: http://127.0.0.1:8000/usersclass/1
